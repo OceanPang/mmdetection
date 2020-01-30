b193ddd7a00a0f89b51811efea91b7e032c6548d
pangjm <pangjiangmiao@gmail.com>
2020-01-30 09:30:15 +0000

    voc

--- mmdet/datasets/voc.py
+++ mmdet/datasets/voc.py
@@ -12,9 +12,10 @@ class VOCDataset(XMLDataset):
 
     def __init__(self, **kwargs):
         super(VOCDataset, self).__init__(**kwargs)
-        if 'VOC2007' in self.img_prefix:
-            self.year = 2007
-        elif 'VOC2012' in self.img_prefix:
-            self.year = 2012
-        else:
-            raise ValueError('Cannot infer dataset year from img_prefix')
+        self.year = 200712
+        # if 'VOC2007' in self.img_prefix:
+        #     self.year = 2007
+        # elif 'VOC2012' in self.img_prefix:
+        #     self.year = 2012
+        # else:
+        #     raise ValueError('Cannot infer dataset year from img_prefix')
